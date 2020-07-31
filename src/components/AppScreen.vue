77053e0f0c3810308abf316a06e11d5d032afbb2
mjbryan <mjbyran10@gmail.com>
2020-07-31 17:55:38 +0200

    tidied scss

--- src/components/AppScreen.vue
+++ src/components/AppScreen.vue
@@ -68,14 +68,12 @@ export default AppScreen;
   border-radius: 5px;
   border: solid black 2px;
   text-align: center;
-  padding: 0 8px; //Added padding to balance page from custom scroller
-  padding-bottom: 50px;
+  padding: 0 8px 50px 8px;
   z-index: 1;
   main {
     min-height: 85vh;
-    margin-bottom: 25vh;
+    margin: 0 auto 25vh auto;
     display: block;
-    margin: 0 auto;
   }
 }
 @media screen and (max-height: 740px) {
