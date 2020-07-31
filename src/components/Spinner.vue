d7a2ca8e83d5782837c0125ae9404bf0b30f1032
mjbryan <mjbyran10@gmail.com>
2020-07-31 17:49:53 +0200

    removed comment

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -43,7 +43,6 @@ export default Spinner;
   margin-left: -($size/2);
   .portal,
   .portal-inner {
-    /* animation: portal-enter 0.2s; */
     width: 100%;
     height: 100%;
     background-size: cover;
